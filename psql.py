3352b1cc2755861d31374282b11dd92465ca041f
Aidan Comer <alhcomer@hotmail.co.uk>
2022-07-14 14:23:35 +0100

    updated psql file

--- psql.py
+++ psql.py
@@ -1,3 +1,4 @@
+from re import sub
 from psycopg2 import sql, Error
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT # <-- ADD THIS LINE
 import psycopg2
@@ -9,13 +10,33 @@ load_dotenv()
 
 # CREATE DATABASE IF ONE DOES NOT EXIST
 
-def insert_table_data(table_name):
-    if table_name == "blog_posts":
-        pass
 
-    if table_name == "portfolio_posts":
-        pass
+# FUNCTION TO CREATE NEW TABLES/ QUERY MUST BE EDITED FOR NEW TABLES
+def create_table():
+    create_table_query = '''CREATE TABLE IF NOT EXISTS portfolio_posts 
+        (id SERIAL PRIMARY KEY NOT NULL,
+        title VARCHAR (250) NOT NULL,
+        subtitle VARCHAR (250) NOT NULL,
+        body TEXT   NOT NULL,
+        created_on TIMESTAMP NOT NULL,
+        img_url TEXT NOT NULL,
+        github_url TEXT)
+        '''
 
+    cursor.execute(create_table_query)
+
+def insert_table_data():
+    title = "creating blog"
+    subtitle = "this is another string"
+    body = "this is the body of the blog"
+    img_url = r"https://upload.wikimedia.org/wikipedia/commons/8/80/Equus_asinus_Kadzid%C5%82owo_001.jpg"
+    query = f'''INSERT INTO blog_posts (title, subtitle, body, img_url)
+    VALUES ({title}, {subtitle}, {body}, {img_url})'''
+
+    cursor.execute(query)
+
+
+# BASE 
 try:
     conn = psycopg2.connect(
         database="my_blog",
@@ -30,21 +51,12 @@ try:
     # Creating a cursor object
     cursor = conn.cursor()
 
-    # SQL query to create blog table
-    create_table_query = '''CREATE TABLE IF NOT EXISTS portfolio_posts 
-        (id SERIAL PRIMARY KEY NOT NULL,
-        title VARCHAR (250) NOT NULL,
-        subtitle VARCHAR (250) NOT NULL,
-        body TEXT   NOT NULL,
-        created_on TIMESTAMP NOT NULL,
-        img_url TEXT NOT NULL,
-        github_url TEXT)
-        '''
+    # PLACE DESIRED INTERACTION WITH DATABASE BELOW
+    insert_table_data()
 
-    cursor.execute(create_table_query)
+    # COMMIT CHANGES
     conn.commit()
 
-    print("Table created successfully in PSQL")
 
     # TEST PSQL BY PRINTING DETAILS
     # print("PostgreSQL server information")
@@ -64,13 +76,11 @@ try:
     # cursor.execute(sql)
     # print("Database has been created successfully !!");
     
-    # Closing the connection
-    # conn.close()
-
 except [Exception, Error] as error:
     print("Error while connecting to PSQL", error)
 
 finally: 
+    # CLOSE CONNECTION
     cursor.close()
     conn.close()
     print("PSQL connection is closed")
