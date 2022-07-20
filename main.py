a9b0d9e923389354d498f02dce31e4624f6dbf4c
Aidan Comer <alhcomer@hotmail.co.uk>
2022-07-20 13:22:08 +0100

    added psql manager closs/jinja implementation

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 from msilib.schema import Error
 from flask import Flask, render_template, url_for, request
-from flask_sqlalchemy import SQLAlchemy
 import os
-from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
-import psycopg2
 from dotenv import load_dotenv
-from psql import get_records
+from psql import PostgresManager
 
 load_dotenv()
 
 # INITIALISE APPLICATION
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('APP_SECRET_KEY')
-
+psql = PostgresManager()
 
 # CONFIGUE ROUTING FOR PAGES
 @app.route('/')
 def home():
-    blog_records = get_records("blog_posts")
-    portfolio_records = get_records("portfolio_posts")
+    blog_records = psql.get_records("blog_posts")
+    print(blog_records)
+    portfolio_records = psql.get_records("portfolio_posts")
     return render_template('index.html', blog_records=blog_records, portfolio_records=portfolio_records)
 
 @app.route('/blog-post/<int:post_id>')
 def show_post(post_id):
-    requested_post = BlogPost.query.get(post_id)
-    if request.method == 'GET':
-        pass
-        
+    pass
+    # requested_post = BlogPost.query.get(post_id)
+    # if request.method == 'GET':
+    #     pass
+
 
 # RUN COMMAND
 if __name__ == '__main__':
--- psql.py
+++ psql.py
@@ -8,116 +8,79 @@ from datetime import datetime, timezone
 
 load_dotenv()
 
-# CREATE DATABASE IF ONE DOES NOT EXIST
+class PostgresManager:
+    def __init__(self):
+        self.connection = None
+        self.cursor = None
 
-
-# FUNCTION TO CREATE NEW TABLES/ QUERY MUST BE EDITED FOR NEW TABLES
-def create_table():
-    create_table_query = '''CREATE TABLE IF NOT EXISTS portfolio_posts 
-        (id SERIAL PRIMARY KEY NOT NULL,
-        title VARCHAR (250) NOT NULL,
-        subtitle VARCHAR (250) NOT NULL,
-        body TEXT   NOT NULL,
-        created_on TIMESTAMP NOT NULL,
-        img_url TEXT NOT NULL,
-        github_url TEXT)
-        '''
-
-    cursor.execute(create_table_query)
-
-def insert_table_data():
-    title = "creating blog"
-    subtitle = "this is another string"
-    body = "this is the body of the blog"
-    dt = datetime.now(timezone.utc)
-    img_url = r"https://upload.wikimedia.org/wikipedia/commons/8/80/Equus_asinus_Kadzid%C5%82owo_001.jpg"
-    query = '''INSERT INTO blog_posts (title, subtitle, body, created_on, img_url)
-    VALUES (%s, %s, %s, %s, %s) RETURNING id;'''
-    
-    cursor.execute(query, (title, subtitle, body, dt, img_url))
-    
-    
-
-
-# BASE 
-try:
-    conn = psycopg2.connect(
-        database="my_blog",
-        user='postgres',
-        password=os.getenv('PSQL_PASSWORD'),
-        host='localhost',
-        port= '5432'
-    )
-    
-    conn.autocommit = True
-    
-    # Creating a cursor object
-    cursor = conn.cursor()
-
-    # PLACE DESIRED INTERACTION WITH DATABASE BELOW
-    insert_table_data()
-
-    # COMMIT CHANGES
-    conn.commit()
-
-
-    # TEST PSQL BY PRINTING DETAILS
-    # print("PostgreSQL server information")
-    # print(conn.get_dsn_parameters(), "\n")
-    # # BY EXERCUTING A SQL QUERY
-    # cursor.execute("SELECT version();")
-    # # FETCH RESULT
-    # record = cursor.fetchone()
-    # print("You are connected to - ", record, "\n")
-
-    
-    
-    # query to create a database 
-    # sql = ''' CREATE database my_blog ''';
-    
-    # executing above query
-    # cursor.execute(sql)
-    # print("Database has been created successfully !!");
-    
-except Error as error:
-    print("Error while connecting to PSQL", error)
-
-finally: 
-    # CLOSE CONNECTION
-    cursor.close()
-    conn.close()
-    print("PSQL connection is closed")
-
-
-#FUNCTION TO GET RECORDS IN MAIN.PY
-def get_records(table_name):
-    try:
-        conn = psycopg2.connect(
+    def open_connection(self):
+        self.connection = psycopg2.connect(
             database="my_blog",
             user='postgres',
             password=os.getenv('PSQL_PASSWORD'),
             host='localhost',
             port= '5432'
         )
-        conn.autocommit = True
-    
-        # Creating a cursor object
-        cursor = conn.cursor()
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor()
 
-        # PLACE DESIRED INTERACTION WITH DATABASE BELOW
-        query = f'''SELECT * FROM {table_name}'''
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-
-        # COMMIT CHANGES
-        conn.commit()
-
-    except Error as error:
-        print("Error while connecting to PSQL", error)
-
-    finally: 
-        # CLOSE CONNECTION
-        cursor.close()
-        conn.close()
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
         print("PSQL connection is closed")
+
+    # BELOW METHOD MUST BE EDITED BEFORE USE
+    def create_table(self):
+        try:
+            self.open_connection()
+            create_table_query = '''CREATE TABLE IF NOT EXISTS portfolio_posts 
+            (id SERIAL PRIMARY KEY NOT NULL,
+            title VARCHAR (250) NOT NULL,
+            subtitle VARCHAR (250) NOT NULL,
+            body TEXT   NOT NULL,
+            created_on TIMESTAMP NOT NULL,
+            img_url TEXT NOT NULL,
+            github_url TEXT)
+            '''
+            self.cursor.execute(create_table_query)
+
+        except Error as error:
+            print("Error while connecting to PSQL", error)
+
+        finally:
+            self.close_connection()
+
+    # BELOW METHOD MUST BE EDITED BEFORE USE
+    def insert_table_data(self):
+        try:
+            self.open_connection()
+            title = "first blog title"
+            subtitle = "first blog subtitle"
+            body = "body of the first blog"
+            dt = datetime.now(timezone.utc)
+            img_url = r"https://upload.wikimedia.org/wikipedia/commons/8/80/Equus_asinus_Kadzid%C5%82owo_001.jpg"
+            query = '''INSERT INTO blog_posts (title, subtitle, body, created_on, img_url)
+            VALUES (%s, %s, %s, %s, %s) RETURNING id;'''
+            self.cursor.execute(query, (title, subtitle, body, dt, img_url))
+
+        except Error as error:
+            print("Error while connecting to PSQL", error)
+
+        finally:
+            self.close_connection()
+
+    def get_records(self, table_name):
+        try:
+            self.open_connection()
+            query = f'''SELECT * FROM {table_name}'''
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+            return result
+
+        except Error as error:
+            print("Error while connecting to PSQL", error)
+
+        finally:
+            self.close_connection()
+
+
