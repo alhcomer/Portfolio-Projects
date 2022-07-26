669f681983673b766a705c473115190a32adaa42
Aidan Comer <alhcomer@hotmail.co.uk>
2022-07-26 21:36:04 +0100

    updated sqlalchemy functionality/deleted psql.py

--- crud.py
+++ crud.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from config import database_uri
-from models import Base, Blog
+from models import Base, Blog, Portfolio
 from datetime import datetime
 
 def recreate_db():
@@ -14,15 +14,39 @@ Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 
-blog = Blog(
+def create_blog_post():
+    blog = Blog(
     img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Donkey_in_Clovelly%2C_North_Devon%2C_England.jpg/640px-Donkey_in_Clovelly%2C_North_Devon%2C_England.jpg',
     title = 'How to create your first blog',
     subtitle = 'It can be a real pain',
-    body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-)
+    body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    s = Session()
+    s.add(blog)
+    s.commit()
+    s.close()
 
-recreate_db()
-s = Session()
-s.add(blog)
-s.commit()
-s.close()
+def create_portfolio_post():
+    portfolio = Portfolio(
+    img_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Donkey_in_Clovelly%2C_North_Devon%2C_England.jpg/640px-Donkey_in_Clovelly%2C_North_Devon%2C_England.jpg',
+    title = 'This Website!',
+    subtitle = 'Created using Flask and SQLAlchemy.',
+    body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    github_url = "https://github.com/alhcomer")
+    s = Session()
+    s.add(portfolio)
+    s.commit()
+    s.close()
+
+def get_blog_posts():
+    s = Session()
+    blog_posts = s.query(Blog).all()
+    s.close()
+    return blog_posts
+
+def get_portfolio_posts():
+    s = Session()
+    blog_posts = s.query(Portfolio).all()
+    s.close()
+    return blog_posts
+
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for, request
 import os
 from dotenv import load_dotenv
 from psql import PostgresManager
+from crud import get_blog_posts, get_portfolio_posts
 
 load_dotenv()
 
@@ -16,9 +17,8 @@ psql = PostgresManager()
 # CONFIGUE ROUTING FOR PAGES
 @app.route('/')
 def home():
-    blog_records = psql.get_records("blog_posts")
-    portfolio_records = psql.get_records("portfolio_posts")
-    print(portfolio_records)
+    blog_records = get_blog_posts()
+    portfolio_records = get_portfolio_posts()
     return render_template(
         'index.html',
         blog_records=blog_records, portfolio_records=portfolio_records
