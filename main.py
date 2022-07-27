9ed93b6213228d96b98fe4d295dbad8801c04ebf
Aidan Comer <alhcomer@hotmail.co.uk>
2022-07-27 12:05:12 +0100

    updated styling and jinja

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from msilib.schema import Error
 from flask import Flask, render_template, url_for, request
 import os
 from dotenv import load_dotenv
-from psql import PostgresManager
 from crud import get_blog_posts, get_portfolio_posts
 
 load_dotenv()
@@ -10,7 +9,6 @@ load_dotenv()
 # INITIALISE APPLICATION
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('APP_SECRET_KEY')
-psql = PostgresManager()
 
 
 
@@ -26,19 +24,17 @@ def home():
 
 @app.route('/blog-post/<int:post_id>')
 def show_blog(post_id):
-    blog_records = psql.get_records("blog_posts")
-    blog_post = None
+    blog_records = get_blog_posts()
     for blog in blog_records:
-        if blog[0] == post_id:
+        if blog.id == post_id:
             blog_post = blog
     return render_template('blog-post.html', blog_post=blog_post)
 
 @app.route('/portfolio-post/<int:post_id>')
 def show_portfolio(post_id):
-    portfolio_records = psql.get_records("portfolio_posts")
-    portfolio_post = None
+    portfolio_records = get_portfolio_posts()
     for record in portfolio_records:
-        if record[0] == post_id:
+        if record.id == post_id:
             portfolio_post = record
     return render_template('portfolio-post.html', portfolio_post=portfolio_post)
 
