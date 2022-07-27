3cfcc68db1e36c625a3ccc9b437c233a056579d6
Aidan Comer <alhcomer@hotmail.co.uk>
2022-07-27 12:54:56 +0100

    implemented wtform

---
+++ forms.py
@@ -0,0 +1,11 @@
+from ast import Sub
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class ContactForm(FlaskForm):
+    name = StringField('Your Name', validators=[DataRequired()])
+    email = StringField('Your Email', validators=[DataRequired(), Email()])
+    subject = StringField('Enter Subject', validators=[DataRequired()])
+    body = TextAreaField('Enter Message Here', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for, request
 import os
 from dotenv import load_dotenv
 from crud import get_blog_posts, get_portfolio_posts
+from forms import ContactForm
 
 load_dotenv()
 
@@ -10,17 +11,20 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('APP_SECRET_KEY')
 
-
-
 # CONFIGUE ROUTING FOR PAGES
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    blog_records = get_blog_posts()
-    portfolio_records = get_portfolio_posts()
-    return render_template(
-        'index.html',
-        blog_records=blog_records, portfolio_records=portfolio_records
-        )
+    if request.method == 'GET':
+        blog_records = get_blog_posts()
+        portfolio_records = get_portfolio_posts()
+        contact_form = ContactForm()
+        return render_template(
+            'index.html',
+            blog_records=blog_records, portfolio_records=portfolio_records,
+            contact_form=contact_form
+            )
+    elif request.method == 'POST':
+        return 'Form posted.'
 
 @app.route('/blog-post/<int:post_id>')
 def show_blog(post_id):
@@ -38,12 +42,5 @@ def show_portfolio(post_id):
             portfolio_post = record
     return render_template('portfolio-post.html', portfolio_post=portfolio_post)
 
-
-# NEED TO ADD CONTACT FORM FUNCTIONALITY
-@app.route('/process_email', methods=['POST'])
-def process_email():
-    pass
-
-# RUN COMMAND
 if __name__ == '__main__':
     app.run(debug=True)
