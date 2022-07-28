b13f47967407ba4ac251c60ff49ef21dc4482732
Aidan Comer <alhcomer@hotmail.co.uk>
2022-07-28 16:32:12 +0100

    began styling wtform

--- forms.py
+++ forms.py
@@ -2,10 +2,12 @@ from ast import Sub
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
+from flask import Markup
 
 class ContactForm(FlaskForm):
     name = StringField('Your Name', validators=[DataRequired()])
     email = StringField('Your Email', validators=[DataRequired(), Email()])
     subject = StringField('Enter Subject', validators=[DataRequired()])
     body = TextAreaField('Enter Message Here', validators=[DataRequired()])
+    # label_for_submit = Markup
     submit = SubmitField('Submit')
